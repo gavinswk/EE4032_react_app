c14748875e10d8c29f042cab664f7b56005a4248
gavinswk <235635214+gavinswk@users.noreply.github.com>
2025-10-29 18:16:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
